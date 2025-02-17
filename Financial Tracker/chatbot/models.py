3380f95bd5147089454795f14a46b98309e020d4
YS <yslee040611@gmail.com>
2025-02-17 13:16:09 +0800

    added venv

--- Financial Tracker/chatbot/models.py
+++ Financial Tracker/chatbot/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    title = models.CharField(max_length=60, null=False)
+    started_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    text = models.TextField()
+    is_user_message = models.BooleanField(default=True)  # True for user, False for AI
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Chatbot():
+    
---
+++ Financial Tracker/chatbot/services.py
@@ -0,0 +1,4 @@
+import budget.repository
+
+
+        
