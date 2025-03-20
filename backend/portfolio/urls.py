dcea453fa1e6007928513759369936b1ddf5f07b
YS <yslee040611@gmail.com>
2025-03-20 16:01:43 +0800

    update budget url

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('budget.urls')),
+    path('budget/', include('budget.urls')),
 ]
