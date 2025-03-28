68ec1c6b7e9e4be002cd591db09abf08af46ce62
YS <yslee040611@gmail.com>
2025-03-28 14:29:56 +0800

    require auth to view

--- backend/budget/views.py
+++ backend/budget/views.py
@@ -1,24 +1,26 @@
 # views.py
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import viewsets, permissions
 from .models import Session, Income, Expense, Bucket, Goals
 from .serializers import SessionSerializer, IncomeSerializer, ExpenseSerializer, BucketSerializer, GoalsSerializer
 
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all().order_by('-period').values()
     serializer_class = SessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class IncomeViewSet(viewsets.ModelViewSet):
     queryset = Income.objects.all()
     serializer_class = IncomeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class BucketViewSet(viewsets.ModelViewSet):
     serializer_class = BucketSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = Bucket.objects.all()
@@ -32,3 +34,4 @@ class BucketViewSet(viewsets.ModelViewSet):
 class GoalsViewSet(viewsets.ModelViewSet):
     queryset = Goals.objects.all()
     serializer_class = GoalsSerializer
+    permission_classes = [permissions.IsAuthenticated]
